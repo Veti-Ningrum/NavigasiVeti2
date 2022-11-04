21b0cd15091d650ab3ad15b915884e836559880a
Veti-Ningrum <veti.ningrum25@gmail.com>
2022-11-04 18:21:35 +0700

    first commit

---
+++ Karyawan.js
@@ -0,0 +1,23 @@
+const karyawan = [
+    {id:'1', nama:'Veti Ningrum', jabatan:'Direktur', 
+    email:'veti_20510010@stimata.ac.id', telp:'083830265245',
+    gaji:'10.000.000', foto:'https://placeimg.com/100/100/people'},
+
+    {id:'2', nama:'kramas', jabatan:'sekretaris', 
+    email:'kramas_olalal@stimata.ac.id', telp:'0893456745678',
+    gaji:'3.000.000', foto:'https://placeimg.com/100/100/nature'},
+
+    {id:'3', nama:'Sarto', jabatan:'bendahara', 
+    email:'sarto_tol@stimata.ac.id', telp:'08123456783',
+    gaji:'3.000.000', foto:'https://placeimg.com/100/100/tech'},
+
+    {id:'4', nama:'obengkarem', jabatan:'wakil direktur', 
+    email:'obengkarem_yes@stimata.ac.id', telp:'08B7875643567831',
+    gaji:'5.000.000', foto:'https://placeimg.com/100/100/arch'},
+
+    {id:'5', nama:'sayutimelik', jabatan:'Office Boy',
+    email:'sayutimelik_owhyaaaaa@stimata.ac.id', telp:'08747885777',
+    gaji:'1.000.000', foto:'https://placeimg.com/100/100/grayscale'},
+];
+
+export default karyawan;
